6fd9f29a8e72d01d368efe46bacaf080d0e23966
Phil de Joux <philderbeast@gmail.com>
2018-07-13 07:19:11 -0400

    Simplify StrictZone.

--- task/library/Flight/Task.hs
+++ task/library/Flight/Task.hs
@@ -4,23 +4,20 @@ import Data.UnitsOfMeasure (u)
 
 import Flight.Zone (Altitude(..))
 
-data Goal
+data StrictZone a where
+    Line :: Eq a => Altitude a [u| m |] -> StrictZone a
 
-data Zone k a where
-    Line :: Eq a => Altitude a [u| m |] -> Zone Goal a
+deriving instance Eq (StrictZone a)
 
-deriving instance Eq (Zone k a)
-
---     /.../task/library/Flight/Task.hs:38:5: error:
+--     /.../task/library/Flight/Task.hs:33:19: error:
 --         • Overlapping instances for Show (Altitude a u0)
 --           Matching givens (or their superclasses):
 --             Show (Altitude a u)
 --               bound by an instance declaration:
---                          forall a (u :: Data.UnitsOfMeasure.Internal.Unit) k.
---                          (Show (Altitude a u),
---                           Show (Radius a (Data.UnitsOfMeasure.Internal.MkUnit "m"))) =>
---                          Show (Zone k a)
---               at library/Flight/Task.hs:(36,5)-(39,22)
+--                          forall a (u :: Data.UnitsOfMeasure.Internal.Unit).
+--                          Show (Altitude a u) =>
+--                          Show (StrictZone a)
+--               at library/Flight/Task.hs:33:19-60
 --           Matching instances:
 --             instance Show (Data.UnitsOfMeasure.Internal.Quantity a u) =>
 --                      Show (Altitude a u)
@@ -29,10 +26,8 @@ deriving instance Eq (Zone k a)
 --         • In the ambiguity check for an instance declaration
 --           To defer the ambiguity check to use sites, enable AllowAmbiguousTypes
 --           In the stand-alone deriving instance for
---             ‘(Show (Altitude a u),
---               Show (Radius a (Data.UnitsOfMeasure.Internal.MkUnit "m"))) =>
---              Show (Zone k a)’
+--             ‘Show (Altitude a u) => Show (StrictZone a)’
 --        |
---     38 |     ( Show (Altitude a u)
---        |     ^^^^^^^^^^^^^^^^^^^^^...             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-deriving instance Show (Altitude a u) => Show (Zone k a)
+--     33 | deriving instance Show (Altitude a u) => Show (StrictZone a)
+--        |                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+deriving instance Show (Altitude a u) => Show (StrictZone a)
